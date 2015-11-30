21644fdb70c516faa2556853e4a6247ca633f499
Chris Wozniak <cwoz117@hotmail.ca>
2015-11-30 14:14:19 -0700

    added data pointer to waveCheck

--- waveCheck.c
+++ waveCheck.c
@@ -24,54 +24,60 @@ static void pullValue(BYTE *source, BYTE *dest, int startIndex, int range){
 	}
 }
 
-void getFile(char *arg, HEAD *head, BYTE *data){
+HEAD* getFile(char *arg, HEAD *head, BYTE *data){
       
-	FILE *fp = fopen(arg, "rb");
+  FILE *fp = fopen(arg, "rb");
       
-      BYTE buff[WORD * 13];
-      BYTE temp[WORD];
-      BYTE halfTemp[WORD/2];
+  BYTE buff[WORD * 13];
+  BYTE temp[WORD];
+  BYTE halfTemp[WORD/2];
 
-	int check = fread(&buff, sizeof(BYTE), sizeof(buff), fp);
+  int check = fread(&buff, sizeof(BYTE), sizeof(buff), fp);
 
-      // Tedius work of setting the struct up for processing.
-      pullValue(&buff[0], &temp[0], 0, sizeof(temp));     // BYTE
-      memcpy(head->chunkID, temp, WORD);
-      
-      pullValue(&buff[0], &temp[0], 4, sizeof(int));     // INT
-      head->chunkSize = makeInt(temp, WORD);
-      
-      pullValue(&buff[0], &temp[0], 8, sizeof(temp));     // BYTE
-      memcpy(head->format, temp, WORD);
-      pullValue(&buff[0], &temp[0], 12, sizeof(temp));    // BYTE
-      memcpy(head->subChunk1ID, temp, WORD);
-      
-      pullValue(&buff[0], &temp[0], 16, sizeof(temp));     // INT
-      head->subChunk1Size = makeInt(temp, WORD);
-      
-      pullValue(&buff[0], &halfTemp[0], 20, sizeof(halfTemp));       // 16 bit
-      head->audioFormat = (short)makeInt(halfTemp, WORD/2);
-      pullValue(&buff[0], &halfTemp[0], 22, sizeof(halfTemp));       // 16 bit
-      head->numChannels = (short)makeInt(halfTemp, WORD/2);
-      
-      pullValue(&buff[0], &temp[0], 24, sizeof(temp));     // INT
-      head->sampleRate = makeInt(temp, WORD);
-      pullValue(&buff[0], &temp[0], 28, sizeof(temp));     // INT
-      head->byteRate = makeInt(temp, WORD);
-      
-      pullValue(&buff[0], halfTemp, 32, sizeof(halfTemp));       // 16 bit
-      head->blockAlign = (short)makeInt(halfTemp, WORD/2);
-      pullValue(&buff[0], halfTemp, 34, sizeof(halfTemp));       // 16 bit
-      head->bitsPerSample = (short)makeInt(halfTemp, WORD/2);
-      pullValue(&buff[0], halfTemp, 36, sizeof(halfTemp));       // 16 bit
-      head->extraVal = (short)makeInt(halfTemp, WORD/2);
-      
-      pullValue(&buff[0], temp, 38, sizeof(temp));    // BYTE
-      memcpy(head->subChunk2ID, temp, WORD);
-      
-      pullValue(&buff[0], temp, 42, sizeof(temp));     // INT
-      head->subChunk2Size = makeInt(temp, WORD);
-      
-      // Get the data Array filled
-	fclose(fp);
+  // Tedius work of setting the struct up for processing.
+  pullValue(&buff[0], &temp[0], 0, sizeof(temp));     // BYTE
+  memcpy(head->chunkID, temp, WORD);
+  
+  pullValue(&buff[0], &temp[0], 4, sizeof(int));     // INT
+  head->chunkSize = makeInt(temp, WORD);
+  
+  pullValue(&buff[0], &temp[0], 8, sizeof(temp));     // BYTE
+  memcpy(head->format, temp, WORD);
+  pullValue(&buff[0], &temp[0], 12, sizeof(temp));    // BYTE
+  memcpy(head->subChunk1ID, temp, WORD);
+  
+  pullValue(&buff[0], &temp[0], 16, sizeof(temp));     // INT
+  head->subChunk1Size = makeInt(temp, WORD);
+  
+  pullValue(&buff[0], &halfTemp[0], 20, sizeof(halfTemp));       // 16 bit
+  head->audioFormat = (short)makeInt(halfTemp, WORD/2);
+  pullValue(&buff[0], &halfTemp[0], 22, sizeof(halfTemp));       // 16 bit
+  head->numChannels = (short)makeInt(halfTemp, WORD/2);
+  
+  pullValue(&buff[0], &temp[0], 24, sizeof(temp));     // INT
+  head->sampleRate = makeInt(temp, WORD);
+  pullValue(&buff[0], &temp[0], 28, sizeof(temp));     // INT
+  head->byteRate = makeInt(temp, WORD);
+  
+  pullValue(&buff[0], halfTemp, 32, sizeof(halfTemp));       // 16 bit
+  head->blockAlign = (short)makeInt(halfTemp, WORD/2);
+  pullValue(&buff[0], halfTemp, 34, sizeof(halfTemp));       // 16 bit
+  head->bitsPerSample = (short)makeInt(halfTemp, WORD/2);
+  pullValue(&buff[0], halfTemp, 36, sizeof(halfTemp));       // 16 bit
+  head->extraVal = (short)makeInt(halfTemp, WORD/2);
+  
+  pullValue(&buff[0], temp, 38, sizeof(temp));    // BYTE
+  memcpy(head->subChunk2ID, temp, WORD);
+  
+  pullValue(&buff[0], temp, 42, sizeof(temp));     // INT
+  head->subChunk2Size = makeInt(temp, WORD);
+  
+  head->data = malloc(head->subChunkSize);
+  if (head->data == NULL){
+    fclose(fp);
+    return null;
+  }
+  
+  // Get the data Array filled
+  fclose(fp);
 }
